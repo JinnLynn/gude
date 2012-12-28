a56b65cf9b29caf88ea6eabfdeb6b3ef8e095b30
JinnLynn <eatfishlin@gmail.com>
2012-12-28 13:25:21 +0800

    强制默认编码为utf-8

--- Gude/__init__.py
+++ Gude/__init__.py
@@ -1,8 +1,12 @@
 ﻿# -*- coding: utf-8 -*-
 import sys, os
-from setting import SCRIPT_PATH
+
+#! 强制默认编码为utf-8
+reload(sys)
+sys.setdefaultencoding('utf8') 
 
 # 使用本地第三方库
+from setting import SCRIPT_PATH
 sys.path.insert(0, os.path.join(SCRIPT_PATH, 'libs'))
 sys.path.insert(0, os.path.join(SCRIPT_PATH, 'plugins'))
 
